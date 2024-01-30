a0d5b5a92fda45305fd09b68095239ba892e7d1f
TUFF <TUFF@DESKTOP-7QOPKB2>
2024-01-31 01:07:37 +0300

    Добавьте файлы проекта.

---
+++ HelloWorldConsoleApp/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using static System.Console;
+
+namespace HelloWorldConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine("Hello World!!! - WriteLine");
+            Write("Hello"); Write(" World"); Write("!!! - Write");
+            ReadKey();
+        }
+    }
+}
